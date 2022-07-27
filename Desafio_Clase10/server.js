64794cf63351fea0ff4f28ef966fab21a515c3c6
Cristian A Correa <cristiancorrea85@gmail.com>
2022-07-27 12:01:12 -0300

    Desafio terminado

--- Desafio_Clase10/server.js
+++ Desafio_Clase10/server.js
@@ -2,6 +2,7 @@
 const Contenedor = require('./contenedor');
 const express = require('express');
 const {Router} = express;
+const handlebars = require('express-handlebars');
 
 const router = Router();
 const app = express(); 
@@ -17,40 +18,77 @@ app.use('/static', express.static(__dirname + '/public'))
 app.use('/api/productos', router)
 
 const mProductArray = [
-    {title: 'Procesador', price: 55000, thumbnail:'https://www.amd.com/es/products/cpu/amd-ryzen-7-1800x', id: 1},
-    {title: 'Monitor', price: 23500, thumbnail:'https://www.pngfind.com/mpng/mobhJ_monitor-definicion-de-monitor-de-computadora-hd-png/', id: 2}
+    {title: 'Procesador', price: 55000, thumbnail:'https://cdn2.iconfinder.com/data/icons/80-s-stuffs-outline/59/Asset_3-128.png', id: 1},
+    {title: 'Monitor', price: 23500, thumbnail:'https://cdn2.iconfinder.com/data/icons/80-s-stuffs-outline/62/Asset_14-512.png', id: 2}
 ]
 
 const mContenedor = new Contenedor(mProductArray);
 
+//#region handlebars
+// app.engine('hbs', handlebars.engine({
+//     extname: '.hbs',
+//     defaultLayout: 'main.hbs',
+//     layoutsDir: __dirname + '/views/hbs',
+//     partialsDir: __dirname + '/views/partials'
+// }));
+
+// app.set('views', './views/hbs');
+// app.set('view engine', 'hbs');
+//#endregion
+
+//#region Pug
+// app.set('views', './views/pug');
+// app.set('view engine', 'pug');
+// //#endregion
+
+//#region ejs
+app.set('views', './views/ejs');
+app.set('view engine', 'ejs');
+//#endregion
+
 router.get('', (req, res) => {
     const mData =  mContenedor.getAll();   
-    return res.json({mData})
+    // return res.json({mData})
+    return res.render('home', {status: 1})
 })
 
-router.get('/:id', (req, res) => {
-    const mSelectedProduct = mContenedor.getById(req.params.id)
-    return res.json({mSelectedProduct})
+router.get('/all', (req, res) => {
+    const mData = mContenedor.data
+    let length = null
+    if(mData.length > 0){
+        length = mData.length
+    }
+    return res.render('productList', {mData, length})
 })
+
+// router.get('/:id', (req, res) => {
+//     const mSelectedProduct = mContenedor.getById(req.params.id)
+//     return res.json({mSelectedProduct})
+// })
 
 router.post('', (req, res) => {
-    mContenedor.save(req.body)
-    return res.send({newProduct: req.body})
+    if(req.body.title && req.body.price && req.body.thumbnail != ''){
+        mContenedor.save(req.body)
+        return res.render('home',{status: 1, hbsStatus: null})
+    }
+    else{
+        return res.render('home',{status: 0, hbsStatus: 'missing data'})
+    }
 })
 
-router.put('/:id', (req, res) => {
-    const mId = Number(req.params.id)
-    const mData = req.body 
-    const mConsulta = products.modifyById(mId, mData)
+// router.put('/:id', (req, res) => {
+//     const mId = Number(req.params.id)
+//     const mData = req.body 
+//     const mConsulta = products.modifyById(mId, mData)
     
-    return res.json({mConsulta})
-})
+//     return res.json({mConsulta})
+// })
 
-router.delete('/:id', (req, res) => {
-    const mId = req.params.id
-    const mData = products.deleteById(mId)
-    return res.json({mData})
-})
+// router.delete('/:id', (req, res) => {
+//     const mId = req.params.id
+//     const mData = products.deleteById(mId)
+//     return res.json({mData})
+// })
 
 
 mServer.on("error", error => console.log(`Error en servidor ${error}`))
