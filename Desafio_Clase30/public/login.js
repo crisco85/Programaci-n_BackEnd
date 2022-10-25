d081a1778e12c6289d521b9aa28b08f3377b7d31
Cristian A Correa <cristiancorrea85@gmail.com>
2022-10-25 11:27:40 -0300

    correción Desafio_Clase30

---
+++ Desafio_Clase30/public/login.js
@@ -0,0 +1,6 @@
+const submitLogin = document.getElementById('submitLogin');
+
+submitLogin.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('Ok Click_Boton');
+})
